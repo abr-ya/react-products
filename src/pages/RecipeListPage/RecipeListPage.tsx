6db88d16d722d74a113ffe0ffcce97d3acd04742
Shkatula Julia <shkatylo4ka@gmail.com>
2025-06-26 21:51:31 +0300

    feat: add recipes table

--- src/pages/RecipeListPage/RecipeListPage.tsx
+++ src/pages/RecipeListPage/RecipeListPage.tsx
@@ -1,5 +1,38 @@
+import { BigBlueSpinner } from "@/components";
+import { Flex, TableContainer, Table, Thead, Tr, Th, Tbody, Td } from "@chakra-ui/react";
+import { recipeApi } from "./recipeApi";
+
 const RecipeListPage = () => {
-  return <div>RecipeListPage</div>;
+  const { data, isLoading } = recipeApi.useGetRecipesListQuery({ page: 1 });
+  const TITLES = ["Название", "Приготовление"];
+
+  if (isLoading) return <BigBlueSpinner />;
+
+  if (!data) return <div>Нет данных</div>;
+
+  return (
+    <Flex>
+      <TableContainer>
+        <Table size="sm">
+          <Thead>
+            <Tr>
+              {TITLES.map((title) => (
+                <Th key={title}>{title}</Th>
+              ))}
+            </Tr>
+          </Thead>
+          <Tbody>
+            {data.map((el) => (
+              <Tr key={el.id}>
+                <Td>{el.name}</Td>
+                <Td>{el.description}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Flex>
+  );
 };
 
 export default RecipeListPage;
