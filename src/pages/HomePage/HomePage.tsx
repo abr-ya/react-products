6c5633c753ec2cffb1cd4af6d62f9f97b7a10492
abr-ya <gamer05@list.ru>
2025-07-03 09:07:48 +0300

    feat: Add Async Select demo

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,41 @@
+import AsyncSelect from "react-select/async";
+
 import { InputWithEdit } from "@/components";
+import axios from "axios";
+import { foodUrl } from "@/api";
+import { IProduct } from "../ProductListPage/productContracts";
 
 const HomePage = () => {
+  const TEST_MODE = true;
+
   const saveHandler = (val: string) => {
     console.log(val);
+  };
+
+  const getCountries = (inputValue: string) => {
+    return axios.get(`${foodUrl}ingredients?like=${inputValue}`).then((res) => {
+      if (TEST_MODE) console.log(res);
+      const options = res.data.map(({ id, name }: IProduct) => ({ label: name, value: id }));
+      if (TEST_MODE) console.log(options);
+
+      return options;
+    });
   };
 
   return (
     <>
       <h1>Home Page</h1>
+      <h2>InputWithEdit</h2>
       <InputWithEdit value="11" onSave={saveHandler} isDigital />
+      <h3>Products == AsyncSelect</h3>
+      <AsyncSelect
+        cacheOptions
+        loadOptions={getCountries}
+        placeholder="Start typing"
+        noOptionsMessage={({ inputValue }) =>
+          inputValue ? "There aren't items for this request" : "Start typing for search"
+        }
+      />
     </>
   );
 };
