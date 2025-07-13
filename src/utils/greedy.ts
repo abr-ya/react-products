98844ca98a62ed3f893b6ebac59f9319fcf20055
abr-ya <gamer05@list.ru>
2025-07-13 16:12:20 +0300

    feat: Add greedy functions

---
+++ src/utils/greedy.ts
@@ -0,0 +1,26 @@
+import { getArraySum } from "./common";
+
+export const oneDayToPacks = (weights: number[], numPacks: number) => {
+  weights.sort((a, b) => b - a);
+  const packs: Array<number[]> = Array.from({ length: numPacks }, () => []);
+  const packWeights = Array(numPacks).fill(0);
+
+  for (const weight of weights) {
+    const minIndex = packWeights.indexOf(Math.min(...packWeights));
+    packs[minIndex].push(weight);
+    packWeights[minIndex] += weight;
+  }
+
+  return packs;
+};
+
+export const packsToUsers = (packs: Array<number[]>) => {
+  const usersPacks: Array<number[]> = Array.from({ length: packs[0].length }, () => []);
+
+  packs.forEach((day) => {
+    usersPacks.sort((a: number[], b: number[]) => getArraySum(a) - getArraySum(b));
+    usersPacks.forEach((user, index) => user.push(day[index]));
+  });
+
+  return usersPacks;
+};
